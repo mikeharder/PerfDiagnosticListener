51d054eb6ca5f217c25f92393398ac410cde7c66
Mike Harder <mharder@microsoft.com>
2017-03-21 22:47:31 -0700

    Add PerfDiagnosticListener

---
+++ PerfDiagnosticListener/PerfDiagnosticListener.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DiagnosticAdapter;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace PerfDiagnosticListener
+{
+    public class PerfDiagnosticListener
+    {
+        private static long _requestsBegan = 0;
+        private static long _requestsEnded = 0;
+        private static long _requestTicks = 0;
+
+        [DiagnosticName("Microsoft.AspNetCore.Hosting.BeginRequest")]
+        public void OnBeginRequest(HttpContext httpContext, long timestamp)
+        {
+            Interlocked.Increment(ref _requestsBegan);
+
+            var requestInfo = new RequestInfo { BeginTimestamp = timestamp };
+            httpContext.Features.Set(requestInfo);
+        }
+
+        [DiagnosticName("Microsoft.AspNetCore.Hosting.EndRequest")]
+        public void OnEndRequest(HttpContext httpContext, long timestamp)
+        {
+            Interlocked.Increment(ref _requestsEnded);
+
+            var requestInfo = httpContext.Features.Get<RequestInfo>();
+            Interlocked.Add(ref _requestTicks, timestamp - requestInfo.BeginTimestamp);
+        }
+
+        public static void Write(TextWriter writer)
+        {
+            var currentRequests = _requestsBegan - _requestsEnded;
+            var secondsPerRequest = (((double)_requestTicks) / _requestsEnded) / Stopwatch.Frequency;
+
+            writer.WriteLine(
+                $"[{DateTime.Now.ToString("HH:mm:ss.fff")}] " +
+                $"CurrentRequests: {currentRequests}\t" +
+                $"RequestsEnded: {_requestsEnded}\t" +
+                $"SecondsPerRequest: {Math.Round(secondsPerRequest, 2)}"
+            );
+        }
+
+        private class RequestInfo
+        {
+            public long BeginTimestamp { get; set; }
+        }
+    }
+}
